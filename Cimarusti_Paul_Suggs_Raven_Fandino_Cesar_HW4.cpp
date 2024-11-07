15e3d91c9dc51567bf8cc939966a01699af2bf44
bsuggsfgcu <bcsuggs3857@eagle.fgcu.edu>
2024-11-07 09:01:31 -0500

    Added empty project files

---
+++ Cimarusti_Paul_Suggs_Raven_Fandino_Cesar_HW4.cpp
@@ -0,0 +1,12 @@
+// Coder/Developer: Paul Cimarusti, Cesar Fandino, & Raven Suggs
+// Class:           COP 2006-80599, Fall 2024
+// Date:            November 7, 2024
+// Description:     C++ Program to generate an amortization table based on given parameters
+// Log:             November 7, 2024, Created project file
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
