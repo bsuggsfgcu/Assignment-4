b23999181dcb7b67a5addd3cab00a2be39ff734c
bsuggsfgcu <bcsuggs3857@eagle.fgcu.edu>
2024-11-07 09:47:40 -0500

    Created data function for loan

--- Cimarusti_Paul_Suggs_Raven_Fandino_Cesar_HW4.cpp
+++ Cimarusti_Paul_Suggs_Raven_Fandino_Cesar_HW4.cpp
@@ -4,9 +4,47 @@
 // Description:     C++ Program to generate an amortization table based on given parameters
 // Log:             November 7, 2024, Created project file
 #include <iostream>
+#include <valarray>
+
+class LoanData
+{
+private:
+    double Bal;
+    double n;
+    double i;
+    double A;
+
+public:
+    LoanData(double p, double years, double inter)
+    {
+        //Number of months (years / 12)
+        n = years / 12;
+        //Interest rate (made into decimal and monthly)
+        i = inter / 100 / 12;
+        //Compute monthly payment using formula
+        A = p*((i*std::pow(i+1, n))/(std::pow(i+1, n) - 1));
+        //Saving balance
+        Bal = p;
+    }
+    void MakePayment(double a)
+    {
+
+    }
+    void PrintAmortizationSchedule()
+    {
+
+    }
+    void PrintPayOffTimeReport()
+    {
+
+    }
+
+};
+
 
 int main()
 {
+    LoanData(100000, 30, 6);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
