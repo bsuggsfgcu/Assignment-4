8828acdee06fd6aba67aeadbcd38c4c8950dc91f
bsuggsfgcu <bcsuggs3857@eagle.fgcu.edu>
2024-11-07 21:02:44 -0500

    Added descriptions to the member functions of the LoanData class

--- Cimarusti_Paul_Suggs_Raven_Fandino_Cesar_HW4.cpp
+++ Cimarusti_Paul_Suggs_Raven_Fandino_Cesar_HW4.cpp
@@ -3,6 +3,7 @@
 // Date:            November 7, 2024
 // Description:     C++ Program to generate an amortization table based on given parameters
 // Log:             November 7, 2024, Created project file and code
+// Sources:         (https://www.geeksforgeeks.org/setw-function-in-cpp-with-examples/?ref=gcse_outind, https://en.cppreference.com/w/cpp/io/manip/left), lines 30, 49, 60, 62, to create and format the tables in LoanData.cpp
 #include <iostream>
 #include "LoanData.h"
 int main()
--- LoanData.cpp
+++ LoanData.cpp
@@ -4,9 +4,15 @@
 #include <iostream>
 #include <valarray>
 
+/**
+ * Creates an object of LoanData type.
+ * @param p The Principle, or starting amount owed.
+ * @param years The number of years to take paying the loan.
+ * @param inter The yearly interest rate.
+ */
 LoanData::LoanData(double p, double years, double inter)
 {
-    //Number of months (years / 12)
+    //Number of months (years * 12)
     LoanData::n = years * 12;
     //Interest rate (made into decimal and monthly)
     LoanData::i = inter / 100 / 12;
@@ -16,11 +22,18 @@ LoanData::LoanData(double p, double years, double inter)
     Bal = p;
 }
 
+/**
+ * Code to make a monthly payment to the amount owed
+ * @param pay Amount paid this month
+ */
 void LoanData::MakePayment(double pay)
 {
     Bal = (i + 1) * Bal - pay;
 }
 
+/**
+ * Generate an amortization schedule to show how many more monthly payments are needed
+ */
 void LoanData::PrintAmortizationSchedule()
 {
     double balance = Bal;
@@ -45,20 +58,25 @@ void LoanData::PrintAmortizationSchedule()
         double principlePaid = payment - iP;
         //ending balance after monthly payment
         const double endingBalance = (i + 1) * balance - payment;
-        //std::cout << balance << " " << iP << " " << principlePaid << " " << endingBalance << " " << std::endl;
         //Code to create data table
         std::cout << "$" << std::setw(tableSpacing) << balance << "$" << std::setw(tableSpacing) << iP << "$" << std::setw(tableSpacing) << principlePaid << "$" << std::setw(tableSpacing) << endingBalance << std::endl;
         balance = endingBalance;
     }
 }
 
+/**
+ * Prints a report which shows how much money that is needed to be paid per month to pay off the loan in a varying number of months.
+ * @param p The principle of the loan.
+ * @param i The monthly interest rate of the loan.
+ */
 void LoanData::PrintPayOffTimeReport(double p, double i)
 {
+    //Table header setup
     std::cout << std::left << std::fixed << std::setprecision(2) << std::setw(20) << "Amount of months" << std::setw(21) << "Monthly payment" << std::endl;
     for(int month = 6; month < 360; month++)
     {
         double payment = p*((i*std::pow(i+1, month))/(std::pow(i+1, month) - 1));
-        //std::cout << month << " " << payment << std::endl;
+        //Code to create table header
         std::cout << std::setw(20) << month << "$" << std::setw(20) << payment << std::endl;
     }
 }
